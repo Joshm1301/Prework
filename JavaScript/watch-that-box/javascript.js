e74e21359c1cd17bff0ea6f4ac0011f049ee97bd
joshm1301 <joshm1301@gmail.com>
2022-05-26 19:55:56 -0500

    JS

---
+++ JavaScript/watch-that-box/javascript.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height="25px;"
+});
+    </script>
